f34efad3cf4ea3f9c283b71ff8014fc950feac0c
Ubuntu <13402506301@163.com>
2022-03-18 10:21:28 +0000

    001 commit

--- mserver.cpp
+++ mserver.cpp
@@ -8,6 +8,20 @@
     sudo kill -9 xxxxx
 
     bind failed. Error: Address already in use
+
+    …or create a new repository on the command line
+    echo "# ubuntu_cpp_socket_server" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/computerchen/ubuntu_cpp_socket_server.git
+    git push -u origin main
+
+    …or push an existing repository from the command line
+    git remote add origin https://github.com/computerchen/ubuntu_cpp_socket_server.git
+    git branch -M main
+    git push -u origin main
     
 */
  
